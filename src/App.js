e38f37f6a26b5e9a169429ec79ff834c81aa2c24
Madimetja Sethosa <madimetjasethosa@gmail.com>
2021-08-27 09:14:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <link
+        rel='stylesheet'
+        href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css'
+        integrity='sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We'
+        crossOrigin='anonymous'
+      />
+      <header className='App-header'>
+        Mad-Soduko
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
