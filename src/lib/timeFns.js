a94f5d0992ce42ff041aa3532bbad2580521bfae
Madimetja Sethosa <madimetjasethosa@gmail.com>
2022-03-06 09:05:43 +0200

    Edited timeFns to incoperate secondsElapsed

--- src/lib/timeFns.js
+++ src/lib/timeFns.js
@@ -4,13 +4,17 @@ function twoDigitFormat (time) {
   return time.length < 2 ? '0' + time : time
 }
 
-/** A function return time in mm:ss given 2 dates/times */
-export function timeFormat (startTime, endTime) {
-  const timeNow = endTime || new Date()
-  const secondsElapsed = parseInt((timeNow - startTime) / 1000)
+export function minutesTimeFormat (secondsElapsed) {
   /* claculate the corresponding seconds from the totalSeconds */
   const seconds = twoDigitFormat((secondsElapsed % 60).toString())
   /* claculate the corresponding minutes from the totalSeconds */
   const mins = twoDigitFormat(Math.floor(secondsElapsed / 60).toString())
   return `${mins}:${seconds}`
+}
+
+/** A function return time in mm:ss given 2 dates/times */
+export function timeFormat (startTime, endTime) {
+  const timeNow = endTime || new Date()
+  const secondsElapsed = parseInt((timeNow - startTime) / 1000)
+  return minutesTimeFormat(secondsElapsed)
 }
